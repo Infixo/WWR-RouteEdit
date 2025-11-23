6b20a3889a7033740337ac7b834bb74c5817140a
Infixo <swidwins@gmail.com>
2025-11-23 22:38:50 +0100

    Adds source methods analyzer for easier patch maintenance.

--- RouteEditMod.cs
+++ RouteEditMod.cs
@@ -1,6 +1,4 @@
-﻿using HarmonyLib;
-using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using Utilities;
 
 namespace RouteEdit;
@@ -8,40 +6,12 @@ namespace RouteEdit;
 
 public static class ModEntry
 {
-    public static string ModName { get; private set; } = nameof(RouteEdit);
-    public static string HarmonyId { get; private set; } = String.Empty;
-
+    public static readonly string ModName = nameof(RouteEdit);
 
     [UnmanagedCallersOnly]
     public static int InitializeMod()
     {
-        DebugConsole.Show();
-
-        // Gather environment info
-        Assembly currentAssembly = Assembly.GetExecutingAssembly();
-        ModName = currentAssembly.GetName().Name ?? nameof(RouteEdit);
-        string? assemblyDirectory = Path.GetDirectoryName(currentAssembly.Location);
-        HarmonyId = "Infixo." + ModName;
-        Log.Write($"Mod {ModName} successfully started. HarmonyId is {HarmonyId}.");
-
-        try
-        {
-            // Harmony
-            var harmony = new Harmony(HarmonyId);
-            //harmony.PatchAll(typeof(Mod).Assembly);
-            harmony.PatchAll();
-            var patchedMethods = harmony.GetPatchedMethods().ToArray();
-            Log.Write($"Plugin {HarmonyId} made patches! Patched methods: " + patchedMethods.Length);
-            foreach (var patchedMethod in patchedMethods)
-                Log.Write($"Patched method: {patchedMethod.DeclaringType?.Name}.{patchedMethod.Name}");
-        }
-        catch (Exception ex)
-        {
-            Log.Write("EXCEPTION. ABORTING.");
-            Log.Write(ex.ToString());
-            return 1;
-        }
-
+        _ = ModInit.InitializeMod(ModName);
         return 0;
     }
 }
